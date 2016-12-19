8bf261be2e786445228f19c5ab7377ec6aacf4fa
Florian Sprang <florian.sprang@tum.de>
2016-12-19 21:10:19 +0100

    Eingabeüberprüfung im c file

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <cmath>
+#include <string>
 
 // TODO: C-Funktion implementieren
 // TODO: ASM-Funktion importieren
@@ -16,30 +18,73 @@
 
 //extern void _asm_do_nothing();
 
+using namespace std;
+
+int exeedLimit(int input){
+    if(input<INT_MIN || input>INT_MAX){
+        return 1;
+    }
+    return -1;
+}
+
+int invalidInput(char* input){
+    //find out length
+    int length=0;
+    while(input[length]!= NULL){
+        length++;
+    }
+
+    //check for invalid character
+    for(int i=0;i<length;i++){
+        if(input[i] < '0' || input[i] > '9'){
+            return 1;
+        }
+    }
+
+    if(input[0]==0 || (input[1] > '0'  && input[1]<='9')){
+        return 1;
+    }
+    return -1;
+}
 
 //auflösung und bereich
 
 int main(int argc, char **argv) {
-    if (argc >5) {
+    if (argc >0) {
         //zeitmessung beginn bei erfolgreichen ausführen des Programmes
-
         //Startzeipunkt setzen
         time_t start;
         time(&start);
 
 
         /*
+         * Parsing commandline arguments and checking for valid input
          * @param r_Start, r_End: Realteil start und Ende
          * @param i_STart, i_End: Imaginärteil Start und Ende
          * @param resolution: Auflösung des Bildes am Ende
          */
-        unsigned int r_Start, r_End, i_Start, i_End, resolution;
+        int r_Start, r_End, i_Start, i_End, resolution;
 
-
-
-
-        //Kommandozeilen argument
+        //Kommandozeilen Argumente Anzahl checken bei flascher Anzahl beenden.
         if(argc == 6) {
+
+           for(int i=1;i<6;i++){
+               if(invalidInput(argv[i])==1){
+                   printf("\nFehlerhafte Eingabe. Bitte geben sie ein /main.c int int int int int. \nProgramm beendet \nErste Stelle");
+                   return -1;
+               }
+           }
+
+            for(int i=1;i<6;i++) {
+                char *error = NULL;
+                long current = strtol(argv[1], &error, 10);
+
+                if (exeedLimit(current) == 1 || *error != '\0') {
+                    printf("\nFehlerhafte Eingabe. Bitte geben sie ein /main.c int int int int int. \nProgramm beendet");
+                    return -1;
+                }
+            }
+
             r_Start = atoi(argv[1]);
             r_End = atoi(argv[2]);
             i_Start= atoi(argv[3]);
@@ -47,14 +92,15 @@ int main(int argc, char **argv) {
             resolution = atoi(argv[5]);
 
 
-            /*
-             * ToDO evtl flascher Input behandlung
-             */
-            if(resolution == 0){
-                printf("Fehlerhafte Eingabe Programm beendet");
+            //Wenn res kleiner gleich 0 ist
+            if(resolution <= 0){
+                printf("\nFehlerhafte Eingabe: Bild sollte hoehere Aufloesung als <=0 haben. Programm beendet");
                 return -1;
             }
 
+        }else{
+            printf("\nFehlerhafter Programmaufruf Programm beendet");
+            return -1;
         }
 
         /*
